a3db27ef546a6bdaefbd0721f583aa08edff1193
Tahir <dibirovtahir@gmail.com>
2022-12-29 11:49:44 +0400

    INIT

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,20 +5,32 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
 const schema = yup.object().shape({
-    productName: yup.string().required(),
+    productName: yup.string().required("Product name can't be empty"),
     unitPrice: yup.number().positive().required(),
     unitsInStock: yup.number().positive().required()
 })
 
 
 export const Form = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const submitForm = (data) =>{
+            console.log(data)
+    }
+
   return (
     <div className='form-container'>
-        <form>
+        <form onSubmit={handleSubmit(submitForm)}>
             <h1>Add Product</h1>
-            <input placeholder='Product name' type="text" name='productName'/>
-            <input placeholder='Unit price' type="number" name="unitPrice" id="" />
-            <input placeholder='Units in stock' type="number" name="unitsInStock" id="" />
+            <input placeholder='Product name' type="text" name='productName' {...register('productName')}/>
+            <p className='error'>{errors.productName?.message}</p>
+            <input placeholder='Unit price' type="number" name="unitPrice" id="" {...register('unitPrice')}/>
+            <p className='error'>{errors.unitPrice?.message}</p>
+            <input placeholder='Units in stock' type="number" name="unitsInStock" id="" {...register('unitsInStock')}/>
+            <p className='error'>{errors.unitsInStock?.message}</p>
             <input type="submit" value="Submit" />
 
         </form>
